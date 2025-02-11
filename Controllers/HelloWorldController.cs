c8c0493d8c059fe2718ff56d40e16530b4008cdb
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-02-11 10:02:17 +0300

    son değişiklikler

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -21,9 +21,18 @@ public class HelloWorldController : Controller
 
   //
   //  GET : /HelloWorld/Welcome/
+  /*
   public string Welcome(string name, int ID = 1)
   {
     return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is {ID}");
+  }
+  */
+
+  public IActionResult Welcome(string name, int numTimes = 1)
+  {
+    ViewData["Message"] = "Hello " + name;
+    ViewData["NumTimes"] = numTimes;
+    return View();
   }
 
   
